8015e797554d6fae0687bd21788e3be1e4bf24b4
Ygon farwynd Quaro <yquaro@at-e4.21-school.ru>
2019-05-14 19:34:02 +0300

    norminating struct_specifier

--- main.c
+++ main.c
@@ -14,151 +14,137 @@
 
 
 
-// int main ()
-// {
-// 	int  nb = -1235;
-// 	char	*str;
-// 	str = "NULL";
-// 	// printf("real:\n");
-// 	// printf("%5.5s\n\n\n", "hello world");
-// 	// printf("\nreal:\n");
-// 	// printf ("len = |%d|\n\n\n",printf("%+10.3d\n\n\n",48));
-// 	// printf ("\nown\n");
-// 	// printf("len = |%d|\n\n\n",ft_printf("%+10.3d\n\n\n", 48));
-// 	// printf("\n\n\n\n\n\n\n");
-// 	// nb = 56432;
-// 	printf("\nreal:\n");
-// 	printf ("len = |%d|\n\n\n",printf("% h"));
-// 	printf ("\nown\n");
-// 	printf("len = |%d|\n\n\n",ft_printf("% h"));
-
-
-
-// 	return (0);
-// }
-
-
-
-
-
-
-
-int main()
+int main ()
 {
-	
-	int		READING_SIZE = 100000;
-	int		INTERNAL_FAILURE = 1;
-	int		pipeans[2];
-	int		pipecor[2];
-	int		ians;
-	int		icor;
-	char	ans[READING_SIZE + 1];
-	char	cor[READING_SIZE + 1];
-	int 	save_out; 
-
-	save_out = dup(STDOUT_FILENO);
-	if (pipe(pipeans) == -1)
-		return (0);
-	if (dup2(pipeans[1], 1) == -1)
-		return (0);
-	if (pipe(pipecor) == -1)
-		return (0);
-	if (dup2(pipecor[1], 2) == -1)
-		return (0);
-
-	int		nb;
-
-	nb = 0;
-	char	*str;
-	str = "NULL";
-
-	ft_printf("test basique:%s\n", str);
-	ft_printf("precision / zero:%.0s\n", str);
-	ft_printf("precision / petite:%.5s\n", str);
-	ft_printf("precision / grande:%.15s\n", str);
-	ft_printf("prec + minus:%-.5s\n", str);
-	ft_printf("size / petite:%5s\n", str);
-	ft_printf("size / grande:%15s\n", str);
-	ft_printf("Minus + size / petite:%-5s\n", str);
-	ft_printf("Minus + size / grande:%-15s\n", str);
-	ft_printf("Minus + size + prec:%-15.5s\n", str);
-	ft_printf("NULL:%12s\n", NULL);
-	ft_printf("NULL:%1s\n", NULL);
-	ft_printf("NULL:%-5.6s\n", NULL);
-	ft_printf("NULL:%-.8s\n", NULL);
-	ft_printf("NULL:%.12s\n", NULL);
-	ft_printf("empty:%-.5s\n", "");
-	ft_printf("empty:%-1.32s\n", "");
-	ft_printf("empty:%1.4s\n", "");
-	ft_printf("empty:%23s\n", "");
-
-	dprintf(2, "test basique:%s\n", str);
-	dprintf(2, "precision / zero:%.0s\n", str);
-	dprintf(2, "precision / petite:%.5s\n", str);
-	dprintf(2, "precision / grande:%.15s\n", str);
-	dprintf(2, "prec + minus:%-.5s\n", str);
-	dprintf(2, "size / petite:%5s\n", str);
-	dprintf(2, "size / grande:%15s\n", str);
-	dprintf(2, "Minus + size / petite:%-5s\n", str);
-	dprintf(2, "Minus + size / grande:%-15s\n", str);
-	dprintf(2, "Minus + size + prec:%-15.5s\n", str);
-	dprintf(2, "NULL:%12s\n", NULL);
-	dprintf(2, "NULL:%1s\n", NULL);
-	dprintf(2, "NULL:%-5.6s\n", NULL);
-	dprintf(2, "NULL:%-.8s\n", NULL);
-	dprintf(2, "NULL:%.12s\n", NULL);
-	dprintf(2, "empty:%-.5s\n", "");
-	dprintf(2, "empty:%-1.32s\n", "");
-	dprintf(2, "empty:%1.4s\n", "");
-	dprintf(2, "empty:%23s\n", "");
-
-
-
-	ians = read(pipeans[0], ans, 700);
-	icor = read(pipecor[0], cor, 700);
-	if ((nb = close(pipeans[0])) < 0)
-		printf ("error3");
-	if ((nb = close(pipecor[0])) < 0)
-		printf ("error4\n");
-	if ((nb = close(pipeans[1])) < 0)
-		printf ("error1\n");
-	if ((nb  = close(pipecor[1])) < 0)
-		printf ("error2\n");
-	ans[ians] = 0;
-	cor[icor] = 0;
-	dup2(save_out, STDOUT_FILENO);
-	
-	int index = 0;
-	while (index < ians || index < icor)
-	{
-		if (ans[index] != cor[index])
-			printf ("%c",ans[index]);
-		index++;
-	}
-	index = 0;
-	printf("\n");
-	while (index < ians || index < icor)
-	{
-		if (ans[index] != cor[index])
-			printf ("%c",cor[index]);
-		index++;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	if (ft_strcmp(cor, ans) == 0)
-	{
-		printf("good\n");
-	}
-	else 
-		printf ("\nERROR\n");
-
-	
+	printf("\nreal:\n");
+	printf ("len = |%d|\n\n\n",printf("% h"));
+	printf ("\nown\n");
+	printf("len = |%d|\n\n\n",ft_printf("% h"));
 	return (0);
 }
+
+
+
+
+
+
+
+// int main()
+// {
+	
+// 	int		READING_SIZE = 100000;
+// 	int		INTERNAL_FAILURE = 1;
+// 	int		pipeans[2];
+// 	int		pipecor[2];
+// 	int		ians;
+// 	int		icor;
+// 	char	ans[READING_SIZE + 1];
+// 	char	cor[READING_SIZE + 1];
+// 	int 	save_out; 
+
+// 	save_out = dup(STDOUT_FILENO);
+// 	if (pipe(pipeans) == -1)
+// 		return (0);
+// 	if (dup2(pipeans[1], 1) == -1)
+// 		return (0);
+// 	if (pipe(pipecor) == -1)
+// 		return (0);
+// 	if (dup2(pipecor[1], 2) == -1)
+// 		return (0);
+
+// 	int		nb;
+
+// 	nb = 0;
+// 	char	*str;
+// 	str = "NULL";
+
+// 	ft_printf("test basique:%s\n", str);
+// 	ft_printf("precision / zero:%.0s\n", str);
+// 	ft_printf("precision / petite:%.5s\n", str);
+// 	ft_printf("precision / grande:%.15s\n", str);
+// 	ft_printf("prec + minus:%-.5s\n", str);
+// 	ft_printf("size / petite:%5s\n", str);
+// 	ft_printf("size / grande:%15s\n", str);
+// 	ft_printf("Minus + size / petite:%-5s\n", str);
+// 	ft_printf("Minus + size / grande:%-15s\n", str);
+// 	ft_printf("Minus + size + prec:%-15.5s\n", str);
+// 	ft_printf("NULL:%12s\n", NULL);
+// 	ft_printf("NULL:%1s\n", NULL);
+// 	ft_printf("NULL:%-5.6s\n", NULL);
+// 	ft_printf("NULL:%-.8s\n", NULL);
+// 	ft_printf("NULL:%.12s\n", NULL);
+// 	ft_printf("empty:%-.5s\n", "");
+// 	ft_printf("empty:%-1.32s\n", "");
+// 	ft_printf("empty:%1.4s\n", "");
+// 	ft_printf("empty:%23s\n", "");
+
+// 	dprintf(2, "test basique:%s\n", str);
+// 	dprintf(2, "precision / zero:%.0s\n", str);
+// 	dprintf(2, "precision / petite:%.5s\n", str);
+// 	dprintf(2, "precision / grande:%.15s\n", str);
+// 	dprintf(2, "prec + minus:%-.5s\n", str);
+// 	dprintf(2, "size / petite:%5s\n", str);
+// 	dprintf(2, "size / grande:%15s\n", str);
+// 	dprintf(2, "Minus + size / petite:%-5s\n", str);
+// 	dprintf(2, "Minus + size / grande:%-15s\n", str);
+// 	dprintf(2, "Minus + size + prec:%-15.5s\n", str);
+// 	dprintf(2, "NULL:%12s\n", NULL);
+// 	dprintf(2, "NULL:%1s\n", NULL);
+// 	dprintf(2, "NULL:%-5.6s\n", NULL);
+// 	dprintf(2, "NULL:%-.8s\n", NULL);
+// 	dprintf(2, "NULL:%.12s\n", NULL);
+// 	dprintf(2, "empty:%-.5s\n", "");
+// 	dprintf(2, "empty:%-1.32s\n", "");
+// 	dprintf(2, "empty:%1.4s\n", "");
+// 	dprintf(2, "empty:%23s\n", "");
+
+
+
+// 	ians = read(pipeans[0], ans, 700);
+// 	icor = read(pipecor[0], cor, 700);
+// 	if ((nb = close(pipeans[0])) < 0)
+// 		printf ("error3");
+// 	if ((nb = close(pipecor[0])) < 0)
+// 		printf ("error4\n");
+// 	if ((nb = close(pipeans[1])) < 0)
+// 		printf ("error1\n");
+// 	if ((nb  = close(pipecor[1])) < 0)
+// 		printf ("error2\n");
+// 	ans[ians] = 0;
+// 	cor[icor] = 0;
+// 	dup2(save_out, STDOUT_FILENO);
+	
+// 	int index = 0;
+// 	while (index < ians || index < icor)
+// 	{
+// 		if (ans[index] != cor[index])
+// 			printf ("%c",ans[index]);
+// 		index++;
+// 	}
+// 	index = 0;
+// 	printf("\n");
+// 	while (index < ians || index < icor)
+// 	{
+// 		if (ans[index] != cor[index])
+// 			printf ("%c",cor[index]);
+// 		index++;
+// 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+// 	if (ft_strcmp(cor, ans) == 0)
+// 	{
+// 		printf("good\n");
+// 	}
+// 	else 
+// 		printf ("\nERROR\n");
+
+	
+// 	return (0);
+// }
--- struct_specifier.c
+++ struct_specifier.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct_specifier.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oargrave <oargrave@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/16 14:54:09 by yquaro            #+#    #+#             */
-/*   Updated: 2019/05/13 16:33:57 by oargrave         ###   ########.fr       */
+/*   Updated: 2019/05/14 19:29:34 by yquaro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,6 @@ const char	find_symb(char *traverse)
 	if (traverse[i] == '\0')
 		return ((char)48);
 	return (traverse[i]);
-}
-
-int			move_after_lh_bigl( )
-{
-	if ( g_spec->big_l == 1 ||  g_spec->l == 1 ||  g_spec->h == 1)
-		return (1);
-	if ( g_spec->ll == 1 ||  g_spec->hh == 1)
-		return (2);
-	return (0);
 }
 
 int			move_after_digits(char *traverse)
@@ -68,22 +59,15 @@ void		struct_specifier(char *traverse)
 	int i;
 
 	i = 0;
-	 g_spec->symb = find_symb(traverse);
+	g_spec->symb = find_symb(traverse);
 	if ( g_spec->symb == 'k') /* флаг для вывода даты. У нее свои особенности, так что флаги типа ll и прочие ей не нужны */
 		return ;
 	while (is_cspdioux_bigx_fegbrk(traverse[i]) != 1 && traverse[i] != '\0')
 	{
 		if (traverse[i] == ' ')
-		{
-			i++;
-			 g_spec->space = 1;
-		}
+			i = read_spaces(traverse, i);
 		if (traverse[i] == '.') /* если точка, то начинается точность */
-		{
-			g_spec->precision = 0;
-			read_digit(traverse + (++i), "precision");
-			i += move_after_digits(traverse + i);
-		}
+			i = read_precision(traverse, i);
 		if (is_calculatesymb(traverse[i]) == 1) /* если засек +-0#, заполняет ими структуру */
 		{
 			read_calculatesymb(traverse[i++]);
@@ -91,7 +75,7 @@ void		struct_specifier(char *traverse)
 		}
 		if (ft_isdigit(traverse[i]) == 1) /* если точки нет, значит цифры - это просто ширина */
 		{
-			read_digit(traverse + i, "width");
+			read_digit(traverse + i, WIDTH);
 			i += move_after_digits(traverse + i);
 		}
 		if (traverse[i] == 'j')
@@ -99,18 +83,10 @@ void		struct_specifier(char *traverse)
 		if (traverse[i] == 'z')
 			g_spec->z = 1;
 		if (is_lh_bigl(traverse[i]) == 1)
-		{
-			read_lh_bigl(traverse + i);
-			i += move_after_lh_bigl();
-		}
+			i = read_lh_bigl(traverse + i, i);
 		if (is_nonsymb(traverse[i]) == 1)
 			i++;
 	}
-	if (traverse[i] != '\0')
-		 g_spec->symb = traverse[i];
-	if ( g_spec->minus == 1)
-		 g_spec->zero = 0;
-	// if ( g_spec->zero == 1)	//fix
-	// 	 g_spec->space = 0;
-	return ;
+	if (g_spec->minus == 1)
+		g_spec->zero = 0;
 }
