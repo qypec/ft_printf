f093c517df12a53e97c26c77c1b8880b573c48e5
Denis-shl <denish13@live.ru>
2019-07-11 18:16:21 +0300

    fix float

--- includes/ft_float.h
+++ includes/ft_float.h
@@ -6,9 +6,9 @@ typedef union
 	float f;
 	struct
 	{
-		int mantisa : 23;
-		short exponent : 8;
-		int sign : 1;
+		unsigned int mantisa : 23;
+		unsigned int exponent : 8;
+		unsigned int sign : 1;
 	}	parts;
 }	float_cast;
 
@@ -18,9 +18,9 @@ typedef union
 	double f;
 	struct
 	{
-		long int mantisa : 52;
-		short exponent : 11;
-		int sign : 1;
+		unsigned long mantisa : 52;
+		unsigned long exponent : 11;
+		unsigned long sign : 1;
 	}   parts;
 }	double_cast;
 
@@ -30,9 +30,9 @@ typedef union
 	long double f;
 	struct
 	{
-		__int128_t mantisa : 112;
-		short exponent : 15;
-		int sign : 1;
+		unsigned long mantisa : 63;
+		unsigned long exponent : 15;
+		unsigned long  sign : 1;
 	}   parts;
 }   long_double_cast;
 
