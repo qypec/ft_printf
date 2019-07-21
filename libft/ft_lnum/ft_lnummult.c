fd8c9103d73a1d1099067493066c6cab443e7635
qypec <qypec.prog@yandex.ru>
2019-07-21 17:02:45 +0300

    WIP ft_lnummult

--- libft/ft_lnum/ft_lnummult.c
+++ libft/ft_lnum/ft_lnummult.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lnummult.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yquaro <yquaro@student.42.fr>              +#+  +:+       +#+        */
+/*   By: qypec <qypec@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/19 16:35:47 by yquaro            #+#    #+#             */
-/*   Updated: 2019/07/19 18:23:47 by yquaro           ###   ########.fr       */
+/*   Updated: 2019/07/21 13:21:28 by qypec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_lnum.h"
+#include <stdio.h>
 
 static void			sum(unsigned char *w, unsigned char **matr)
 {
@@ -20,7 +21,8 @@ static void			sum(unsigned char *w, unsigned char **matr)
 	while (matr[i] != NULL)
 	{
 		if (matr[i + 1] != NULL)
-			ft_lnumadd(w, matr[i], matr[i + 1], ft_strlen(matr[i]))
+			ft_lnumadd(w, matr[i], matr[i + 1], \
+					ft_strlen((const char *)matr[i]));
 		i++;
 	}
 }
@@ -42,11 +44,12 @@ void				ft_lnummult(unsigned char *w, unsigned char *u, \
 							(n + i + 1))) == NULL)
 			exit(-1);
 		ft_bzero(matr[i], n + i + 1);
+		i++;
 	}
 	matr[i] = NULL;
 	i = 0;
 	j = n - 1;
-	while (matr[i] != NULL)
+	while (j)
 	{
 		ft_lnumsmult(matr[i], u, INT(v[j]), n);
 		i++;
@@ -63,8 +66,8 @@ int main()
 	int					i;
 
 	w = (unsigned char *)ft_strdup("00000000000000");
-	v = (unsigned char *)ft_strdup("0100010");
+	u = (unsigned char *)ft_strdup("0100010");
 	v = (unsigned char *)ft_strdup("0111111");
-	ft_lnumult(w, u, v , 7);
+	ft_lnummult(w, u, v , 7);
 	printf("-> %s\n", w);
 }
