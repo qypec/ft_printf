d12417b113e9df5f20580b9cd00ca6b758e54cd8
Denis-shl <denish13@live.ru>
2019-07-23 13:55:50 +0300

    not lnum

--- src/rounding.c
+++ src/rounding.c
@@ -5,51 +5,47 @@
 #include <string.h>
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
-
-
 # define FLAG   0
 char *ft_rounding(char *number)
 {
 	int i;
 	int j;
 	int size;
-	char *num = NULL;
+	char *result = NULL;
 	int precision;
 
 	precision = -1;
 
 	i = 0;
 	j = 0;
-	
-	size = strlen(number);
-	if (!(num = (char *)malloc(sizeof(char) * (size + 2))))
+	size++;
+	while (number[size] != '.' || number[size] != '\0')
+		size++;
+	if (!(result = (char *)malloc(sizeof(char) * (size + precision + 1))))
 		return (NULL);
-	num[size + 1] = '\0';
-	num[0] = '0';
-	num++;
 	/* создаём массив на 1 больше*/
-	strcpy(num, number);
-	num--;
+	strcpy(result, number);
+	result--;
 	
-	if (num == NULL)
+	if (result == NULL)
 		return (NULL);
 		/*идем до точки  */
-	while (num[i] != '.')
+	while (result[i] != '.')
 		i++;
 		/*Смотрим до какого разряда нужно округлить */
 	while (i < precision)
 		i++;
 		/*округляем разряд в большую часть если след разряд больше 4 */
-	if (num[i - 1] - 48 > 4 && num[i - 1] - 48 <= 8)
+	if (result[i - 1] - 48 > 4 && result[i - 1] - 48 <= 8)
 	{
-		num[i - 1] = ROUDING(num[i - 1]);
-		num[i] = '\0';
+		result[i - 1] = ROUDING(result[i - 1]);
+		result[i] = '\0';
 	}
-	if (num[i - 1] - 48 == 9)
+	if (result[i - 1] - 48 == 9)
 	{
 		
 	}
-	return (num);
+	return (result);
 }
 
 int main ()
