ba64806c595973f46c4530a9b0058806e118c272
qypec <qypec.prog@yandex.ru>
2019-07-30 23:20:09 +0300

    rewrited ft_buffer

--- libft/ft_buffer/ft_buffer.h
+++ libft/ft_buffer/ft_buffer.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_buffer.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oargrave <oargrave@student.42.fr>          +#+  +:+       +#+        */
+/*   By: qypec <qypec@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/09 13:10:59 by qypec             #+#    #+#             */
-/*   Updated: 2019/07/30 20:27:13 by oargrave         ###   ########.fr       */
+/*   Updated: 2019/07/30 23:19:33 by qypec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,8 @@ typedef struct			s_buff
 }						t_buff;
 
 t_buff					*ft_buffinit(int size);
-void					ft_buff_addstr(t_buff *buff, char *str); // добавляет строку в буфер
-void					ft_buff_addsymb(t_buff *buff, char symb); // добавляет символ в буфер 
-// void					ft_buffreload(t_buff *buff);
+void					ft_buffladd(t_buff *buff, const char *str, int len);
+void					ft_buffadd(t_buff *buff, const char *str);
 void					ft_buffdel(t_buff **buff);
 
 
