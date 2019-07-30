c2427221a5cfad2f02505fde4623e4e049d439d2
qypec <qypec.prog@yandex.ru>
2019-07-31 00:57:08 +0300

    added description for ft_buffer

--- libft/ft_buffer/ft_buffer.h
+++ libft/ft_buffer/ft_buffer.h
@@ -6,7 +6,7 @@
 /*   By: qypec <qypec@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/09 13:10:59 by qypec             #+#    #+#             */
-/*   Updated: 2019/07/30 23:19:33 by qypec            ###   ########.fr       */
+/*   Updated: 2019/07/31 00:56:22 by qypec            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,21 @@ typedef struct			s_buff
 }						t_buff;
 
 t_buff					*ft_buffinit(int size);
-void					ft_buffladd(t_buff *buff, const char *str, int len);
 void					ft_buffadd(t_buff *buff, const char *str);
+void					ft_buffladd(t_buff *buff, const char *str, int len);
 void					ft_buffdel(t_buff **buff);
+/*
+	Пример: 
+		t_buff *buff;
+
+		buff = ft_buffinit(100); // инициализировали буфер с размером 100. В случае переполнения буфера
+							размер будет увеличиваться на это же число
+		ft_buffadd(buff, "hello world"); // положили строку в буфер
+		ft_buffadd(buff, "!"); 			// положили символ в буфер
+		ft_buffladd(buff, "example", 7); // положили строку "example" 
+					в буфер и указали размер этой строки, чтобы в функции он снова не считался
+		ft_buffdel(&buff);
+*/
 
 
 #endif
