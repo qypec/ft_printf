a1e7718eb633cb8536acca7ad69d1f1c250941cd
Denis-shl <denish13@live.ru>
2019-07-30 22:22:00 +0300

    new buff and float

--- src/print_float.c
+++ src/print_float.c
@@ -6,133 +6,97 @@
 /*   By: oargrave <oargrave@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 13:43:54 by oargrave          #+#    #+#             */
-/*   Updated: 2019/07/30 20:16:40 by oargrave         ###   ########.fr       */
+/*   Updated: 2019/07/30 22:21:14 by oargrave         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "../includes/header.h"
 
-
-static char*	width_float_two(char *result, int size_result, int i, int j)
+void	getnumber(t_part *part, long double num)
 {
-	char *result_new;
-	
-	result_new = (char *)malloc(sizeof(char) * g_spec->width + 1);
-	if (g_spec->zero == 1 && !(g_spec->minus))
+	unsigned long long number;
+	int i;
+
+	i = 0;
+	number = (unsigned long long)num;
+	ft_buff_addstr (part->middle, ft_itoa_u(number));
+	ft_buff_addsymb(part->middle, '.');
+	while (i < g_spec->precision + 3)
 	{
-		if (g_spec->is_neg)
-			result_new[i++] = '-';
-		if (!(g_spec->is_neg) && g_spec->plus)
-			result_new[i++] = '+';
-		while (i < g_spec->width)
-			result_new[i++] = '0';
+		number = (double)num * 10;
+		num *= 10;
+		num -= number;
+		ft_buff_addsymb(part->middle, number + 48);
+		i++;
 	}
-	else if (!(g_spec->minus) || g_spec->is_neg)
+}
+
+void getfirst(t_part *part)
+{
+	int size_zero;
+	int size_middle;
+	int i;
+ 
+	i = 0;
+	if (g_spec->minus == 0)
 	{
-		while (i < g_spec->width)
-			result_new[i++] = ' ';
-	}
-	else if (g_spec->minus )
-	{
+		if (g_spec->zero)
+		{
+			if (g_spec->plus && !(part->is_neg))
+				part->first[i++] = '-';
+			if (part->is_neg)
+				part->first[i++] = '-';
+			size_middle = ft_strlen(part->middle->str);
+			size_zero = g_spec->width - size_middle;
+			if ((part->first = (char *)malloc(sizeof(char) * size_zero)) == NULL)
+				exit(-1);
+			while (i < size_zero)
+			{
+				part->first[i] =  '0';
+				i++;
+			}
+		}
+		else
+		{
+			size_middle = ft_strlen(part->middle->str);
+			size_zero = g_spec->width + 1 - size_middle;
+			if ((part->first = (char *)malloc(sizeof(char) * size_zero)) == NULL)
+				exit(-1);
+			while (i < size_zero)
+			{
+				part->first[i] =  ' ';
+				i++;
+			}
+			if (g_spec->plus && !(part->is_neg))
+				part->first[i++] = '-';
+			if (part->is_neg)
+				part->first[i++] = '-';
+		}
 		
 	}
-	return (result_new);
 }
 
-static char*	width_float(char *result, int size_result)
+void getend(t_part *part)
 {
-	int size;
+	int num_indents;
+	int size_buff;
 	int i;
-	int j;
+
+	size_buff = ft_strlen(part->middle->str) + ft_strlen(part->first);
+	num_indents = g_spec->width - size_buff;
+	i = 0;
+	if (g_spec->minus == 0)
+		part->last = NULL;
+	else
+	{
+		while (i < num_indents)
+		{
+			ft_buff_addsymb(part->middle, ' ');
+			i++;
+		}
+	}
 	
-	i = 0;
-	j = 0;
-	if (g_spec->is_neg || (g_spec->plus && !(g_spec->is_neg)))
-		size_result++;
-	if (g_spec->width <= size_result)
-	{
-		if (g_spec->is_neg)
-			addsymb_glbuffer('-');
-		if (!(g_spec->is_neg) && g_spec->plus)
-			addsymb_glbuffer('+');
-		return (result);
-	}
-	else if (g_spec->width > size_result)
-		result = width_float_two(result, size_result, i, j);
-	return (result);		
-}
-
-static void	float_copy(char *numb, char *str, int i, int size_str)
-{
-	int j;
-	char *result;
-	int size_result;
-
-	j = 0;
-	if (!(result = (char *)malloc(sizeof(char) * (i + size_str + 3))))
-		return ;
-	i = 0;
-	while (str[j] != '\0')
-		result[i++] = str[j++];
-	j = 0;
-	while (numb[j] != '\0')
-		result[i++] = numb[j++];
-	result[i] = '\0';
-	ft_rounding(result, i);
-	size_result = ft_strlen(result);
-	result = width_float(result, size_result);
-	update_glbuffer(result);
-	free(str);
-	free(numb);
-	free(result);
-}
-
-void	print_float(long double num, int i, int j, int size_str)
-{
-	unsigned long long number;
-	char *str;
-	char *numb;
-	char *result;
-
-	number = (unsigned long long)num;
-	str = ft_itoa_u(number);
-	size_str = ft_strlen(str);
-	num -= number;
-	numb = (char *)malloc(sizeof(char) * (size_str + g_spec->precision + 3));
-	numb[i++] = '.';
-	while (i < g_spec->precision + 3)
-	{
-		number = (double)num * 10;
-		num *= 10;
-		num -= number;
-		numb[i] = number + 48;
-		i++;
-	}
-	numb[i] = '\0';
-	float_copy(numb, str, i, size_str);
-}
-
-char	*getnumber(t_part *part, long double num)
-{
-	unsigned long long number;
-
-	number = (unsigned long long)num;
-	str = ft_itoa_u(number);
-	size_str = ft_strlen(str);
-	num -= number;
-	numb = (char *)malloc(sizeof(char) * (size_str + g_spec->precision + 3));
-	numb[i++] = '.';
-	while (i < g_spec->precision + 3)
-	{
-		number = (double)num * 10;
-		num *= 10;
-		num -= number;
-		numb[i] = number + 48;
-		i++;
-	}
-	numb[i] = '\0';
-	float_copy(numb, str, i, size_str);
 }
 
 void	print_float1(va_list arg)
@@ -153,6 +117,7 @@ void	print_float1(va_list arg)
 		part->is_neg = 1;
 	}
 	getnumber(part, num);
+	ft_rounding(part->middle->str, ft_strlen(part->middle->str));
 	getfirst(part);
 	getend(part);
 }
